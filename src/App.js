b9bf13aed06675f9955d3afadc7f853b4ed2fdae
Prakhar-Srivastava <18552460+Prakhar-Srivastava@users.noreply.github.com>
2020-11-02 02:23:37 +0530

    Functional UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import XLSX from 'xlsx';
-import moment from 'moment';
+import {useState} from 'react';
 
 
-const remote = false, 
+const remote = true, 
 	URL = remote ? 'https://scenic-crossbar-287116.el.r.appspot.com/' : 'http://localhost:8080/';
 
 function readHandler({target}){
 
 	alert("You file was short and is processed without the uplod");
 
-	const wb = XLSX.read(target.result, {type: "binary"}),
+	const wb = XLSX.read(target.result, {type: "binary", cellDates: true}),
 		data = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]);
 	
 	let invoices = new Set();
@@ -25,7 +25,7 @@ function readHandler({target}){
 			'Vendor name'
 		]
 		.reduce((last, el) => last && el in row && row[el] !== undefined, true), 
-		validDate = moment(row['Pstng Date'], 'dd/mm/yyyy').unix() <= Date.now(),
+		validDate = row['Pstng Date'].valueOf() <= Date.now(),
 		seenAlready = invoices.has(row['Invoice Numbers']);
 
 		if(!seenAlready)
@@ -34,7 +34,7 @@ function readHandler({target}){
 		return !seenAlready && mustHaves && validDate;
 	});
 
-	fetch(URL, {
+	return fetch(URL, {
 		method: 'POST',
 		mode: 'cors',
 		cache: 'no-cache',
@@ -46,13 +46,25 @@ function readHandler({target}){
 		redirect: 'follow',
 		referrerPolicy: 'no-referrer',
 		body: JSON.stringify({invoices: valid})
-	}).then(resp => resp.json()
+	})/*.then(resp => resp.json()
 			.then(d => console.log(d))
 			.catch(err => console.error(err))
-	).catch(err => console.error(err));
+	).catch(err => console.error(err));*/
+}
+
+function fileUploader(file) {
+	let fd = new FormData();
+	fd.append('invoice_sheet', file);
+	return fetch(`${URL}upload`, {method: 'POST', body: fd})
+	// .then(resp => resp.json()
+	// 	.then((data) => console.log(data))
+	// 	.catch((err) => console.error(err))
+	// ).catch((err) => console.error(err));
 }
 
 function App() {
+
+	const [data, setData] = useState(null);
 
 	const handleUpload = (e) => {
 		const {files} = e.target;
@@ -60,19 +72,33 @@ function App() {
 		if(files && files[0]){
 
 			for(const file of files){
-				const reader = new FileReader();
-				reader.onloadend = readHandler;
-				reader.readAsBinaryString(file);
+				if(file.size < 50 * 1024 * 1024) {
+					const reader = new FileReader();
+					reader.onloadend = (e) => {
+						readHandler(e)
+						.then((resp) => resp.json()
+							.then((d) => setData(d))
+							.catch((err) => console.error(err))
+						).catch((err) => console.error(err));
+					};
+					reader.readAsBinaryString(file);
+				} else
+					fileUploader(file)
+					.then((resp) => resp.json()
+						.then((d) => setData(d))
+						.catch((err) => console.error(err))
+					).catch((err) => console.error(err));
 			}
 		}
 	};
 
 	return (
 		<div className="App">
-			<form runat="server" action={`${URL}upload`} encType="multipart/form-data" method="POST">
-				<input type="file" multiple accept=".xls" onChange={handleUpload} name="invoice_sheet" />
-				<input type="submit" />
+			<form>
+				<input type="file" accept=".xls" onChange={handleUpload} name="invoice_sheet" />
+				<input type="reset" />
 			</form>
+			{data ? JSON.stringify(data) : null}
 		</div>
 	);
 }
